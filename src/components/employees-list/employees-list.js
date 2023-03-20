520ec870714cd485061f826e573a8a382c916ddc
dzoric1 <Gerasimov2099@mail.ru>
2023-03-20 13:05:12 +0800

    first commit

---
+++ src/components/employees-list/employees-list.js
@@ -0,0 +1,14 @@
+import EmployeesListItem from "../employees-list-item/employees-list-item";
+import './employees-list.css';
+
+const EmployeesList = () => {
+  return (
+    <ul className="app-list list-group">
+      <EmployeesListItem name="John C." salary={800} />
+      <EmployeesListItem name="Alex M." salary={3000} />
+      <EmployeesListItem name="Carl W." salary={550} />
+    </ul>
+  )
+}
+
+export default EmployeesList;
