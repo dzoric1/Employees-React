25dcf84b6a052ce4f9d1d531c0ddda1431715c20
dzoric1 <Gerasimov2099@mail.ru>
2023-03-21 20:45:47 +0800

    realized addForm

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,4 @@
+import { Component } from 'react';
 import AppInfo from '../app-info/app-info';
 import AppFilter from '../app-filter/app-filter';
 import SearchPanel from '../search-panel/search-panel';
@@ -6,26 +7,60 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: 'John C.', salary: 800, increase: false, id: 1 },
+        { name: 'Alex M.', salary: 3000, increase: true, id: 2 },
+        { name: 'Carl W.', salary: 5000, increase: false, id: 3 },
+      ]
+    }
+    this.maxId = 4;
+  }
 
-  const data = [
-    { name: 'John C.', salary: 800, increase: false, id: 1 },
-    { name: 'Alex M.', salary: 3000, increase: true, id: 2 },
-    { name: 'Carl W.', salary: 5000, increase: false, id: 3 },
-  ]
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      return {
+        data: data.filter(item => item.id !== id)
+      }
+    })
+  }
 
-  return (
-    <div className="app">
-      <AppInfo />
+  addItem = (name, salary) => {
+    const newItem = {
+      name,
+      salary,
+      increase: false,
+      id: this.maxId++
+    }
 
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
-        <EmployeesList data={data} />
-        <EmployeesAddForm />
+    this.setState(({ data }) => {
+      const newArr = [...data, newItem]
+      return {
+        data: newArr
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="app" >
+        <AppInfo />
+
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+          <EmployeesList
+            data={this.state.data}
+            onDelete={this.deleteItem} />
+          <EmployeesAddForm
+            onAdd={this.addItem} />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App;
